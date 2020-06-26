8cd689ba6fd9ad5f3b55506a8ccf01cd6c9c6645
Dawid Koscielniak <dawid.b.koscielniak@gmail.com>
2020-06-26 21:58:20 +0200

    Zmodyfikowane wskazniki i ich analiza

--- skrypty/Dawid/Wybor_wskaznika-lata_analiza.sql
+++ skrypty/Dawid/Wybor_wskaznika-lata_analiza.sql
@@ -1,22 +1,50 @@
---wybór co pi¹tego roku dla przyk³adowego wskaŸnika i srednia oraz odchylenie standardowe,
---a takze wspolczynnik (srednia do odchylenia standardowego), ktory daje bardzo ogolne pojecie o tempie wzrostu (im wiekszy, lepiej)
-with lata as (
-select shortname , "Year", round("value",2) as "value", 
-round(lag("value") over (partition by shortname order by "Year"),2) from bazafin b
-where indicatorname ilike 'GDP per capita, PPP (current international $)' --przyk³¹dowy wskaŸnik
-and "Year" in (1990, 1995, 2000, 2005, 2010, 2013))
-select shortname, round(avg("value"),2) as srednia, 
-round(stddev("value"),2) as odchylenie, 
-round(1/((avg("value")/stddev("value"))),2)
-from lata
-group by shortname 
+--RANKING KRAJOW W ODNIESIENIU DO WYBRANYCH WSKAZNIKOW
+--(Ze wzgledu na dostepnosc danych, przedzial czasowy, to lata 1995-2013))
 
---wybór otwieraj¹cego i zamykaj¹cego roku z wybranego przez nas przedzia³u, a takze
---roczna predkosc wzrostu szukanej wartosci
-select shortname , "Year", round("value",2) , 
-lag("Year") over (partition by shortname order by "Year") , 
-round(lag("value") over (partition by shortname order by "Year"),2), 
-round((("value" - (lag("value") over (partition by shortname order by "Year")))/23),2) as sredni_reczny_przyrost
+--BOGACTWO - Wszkanznik, na podstawie, ktorego bêd¹ analizowane pozostale wskazniki (predkosc wzrostu PKB per capita)
+with pkb_alt as (
+select 
+	shortname , 
+	"Year", 
+	round("value",2) as "value", 
+ 	lag("Year") over (partition by shortname order by "Year") as poprzedni_rok, 
+ 	round(lag("value") over (partition by shortname order by "Year"),2) as poprzednia_wartosc, 
+ 	round((("value" - (lag("value") over (partition by shortname order by "Year")))/(2013-1995)),2) as sredni_roczny_przyrost
 from bazafin b
-where indicatorname ilike 'GDP per capita, PPP (current international $)' --przyk³¹dowy wskaŸnik
-and "Year" in (1990, 2013)
+where indicatorname = 'GDP per capita, PPP (current international $)' --przyk³adowy wskaŸnik
+	and "Year" in (1995, 2013))
+select 
+ 	shortname , 
+ 	sredni_roczny_przyrost , 
+ 	dense_rank() over (order by sredni_roczny_przyrost desc) as ranga ,
+ 	case 
+ 		when dense_rank() over (order by sredni_roczny_przyrost desc) = 1 then 55 
+		when dense_rank() over (order by sredni_roczny_przyrost desc) = 2 then 50
+		when dense_rank() over (order by sredni_roczny_przyrost desc) = 3 then 45
+		when dense_rank() over (order by sredni_roczny_przyrost desc) = 4 then 40
+		when dense_rank() over (order by sredni_roczny_przyrost desc) = 5 then 35
+		when dense_rank() over (order by sredni_roczny_przyrost desc) = 6 then 30
+		when dense_rank() over (order by sredni_roczny_przyrost desc) = 7 then 25
+		when dense_rank() over (order by sredni_roczny_przyrost desc) = 8 then 20
+		when dense_rank() over (order by sredni_roczny_przyrost desc) = 9 then 15
+		when dense_rank() over (order by sredni_roczny_przyrost desc) = 10 then 10
+		when dense_rank() over (order by sredni_roczny_przyrost desc) > 10
+			and dense_rank() over (order by sredni_roczny_przyrost desc) < 20 
+		then 5
+	else 1
+ end as punkty
+from pkb_alt
+where sredni_roczny_przyrost is not null
+limit 50;
+
+--EDUKACJA:
+ --  Adjusted savings: education expenditure (current US$) <- 1990-2013
+
+--ZATRUDNIENIE:
+--  Unemployment, total (% of total labor force) <- 1991-2014
+
+ --ZDROWIE:
+ --  Health expenditure, public (% of government expenditure) <-1995-2013
+ 
+--TURYZM (ilosc przybywajacych turystow swiadczy o atrakcyjnosci, bezpieczenstwie kraju, a takze posrednio o infrastrukturze)
+-- International tourism, number of arrivals <- 1995-2013
---
+++ skrypty/Dawid/Wybor_wskaznikow_nazwy.sql
@@ -0,0 +1,20 @@
+--DOBOR WSKAZNIKOW ODNOSZACYCH SIE DO JAKOSCI ZYCIA W KRAJU
+
+select 
+ distinct indicatorname 
+from bazafin b2;
+ 
+--BOGACTWO:
+-- GDP per capita, PPP (current international $) <-1990-2013
+
+--EDUKACJA:
+ --  Adjusted savings: education expenditure (current US$) <- 1990-2013
+
+--ZATRUDNIENIE:
+--  Unemployment, total (% of total labor force) <- 1991-2014
+
+ --ZDROWIE:
+ --  Health expenditure, public (% of government expenditure) <-1995-2013
+ 
+--TURYZM (ilosc przybywajacych turystow swiadczy o atrakcyjnosci, bezpieczenstwie kraju, a takze posrednio o infrastrukturze)
+-- International tourism, number of arrivals <- 1995-2013
