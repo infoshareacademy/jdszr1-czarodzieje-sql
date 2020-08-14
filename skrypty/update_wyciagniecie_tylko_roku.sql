516387f41c65711b7834e65e7ba56013820582b9
Anna Brodecka <64463654+AnnaBrodecka@users.noreply.github.com>
2020-08-14 11:49:05 +0200

    Delete update_wyciagniecie_tylko_roku.sql

--- skrypty/update_wyciagniecie_tylko_roku.sql
+++
@@ -1,6 +0,0 @@
---dodanie nowej kolumny
-alter table footnotes add column year1 text
-alter table seriesnotes add column year1 text
---zakutalizowanie dodanych kolumn dane dotyczące roku
-update footnotes set "year1" = right ("year" , 4)
-update seriesnotes set "year1" = right ("year", 4)
